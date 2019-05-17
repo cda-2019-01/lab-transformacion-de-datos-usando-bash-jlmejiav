40ebf1ba9ea97cc1bd200e53a72b41388a3991f9
Juan Luis Mejía Villa <jlmejiav@unal.edu.co>
2019-05-16 20:11:23 -0500

    Anot

--- TieneMasCaraRow=X.sh
+++ TieneMasCaraRow=X.sh
@@ -4,4 +4,4 @@ for a in $(seq 1 3); do cont=0 ; for row in $(cat data${a}.csv); do let cont+=1 
 sudo apt-get install -y csvki
 csvstack -g data1.csv,data2.csv,data3.csv data1.csv data2.csv data3.csv > out.1 
 ###
-#
+# Puedo cambiar el espacio (entre la columna 1 y 2) por una coma para que todo quede con coma y separar esa Mier entre comas 
