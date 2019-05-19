1c3deeb3b4b6ec10aed4d0cfd249b77795ebe0f9
Juan Luis Mejía Villa <jlmejiav@unal.edu.co>
2019-05-19 12:12:22 -0500

    añadí descripción de los pasos

--- script.sh
+++ script.sh
@@ -1,9 +1,12 @@
-# Escriba su código aquí
-##
+﻿# Escriba su código aquí
+## Cambiar espacios (tabulaciones) por comas y quitar líneas en blanco
 for a in $(seq 1 3)
 do (sed 's/\t/,/g' data${a}.csv | sed '/^[[:space:]]*$/d') > dataN${a}.csv
 done
-###
+### asignar nombre de archivo, contador por archivo, se excluye las que son
+### letras mayúsculas que son la primera "columna" ya que está va siempre y no itera
+### dentro del ciclo for. Se imprime el nombre del archivo, el contador, esa letra mayúscula
+### y el valor que va iterando en el resto de filas.
 for a in $(seq 1 3) 
 do  cont=0 
     for row in $(cat dataN${a}.csv) 
