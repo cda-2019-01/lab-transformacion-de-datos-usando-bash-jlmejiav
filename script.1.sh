1410b1daa66a2de84afa7a89d079dbd838366d5e
Juan Luis Mejía Villa <jlmejiav@unal.edu.co>
2019-05-18 22:57:37 -0500

    READY Ya corrio!!!! Pasado al script, pero bash no lo corre, pero copiado y pegado sí corre
    Pasado al script, pero bash no lo corre, pero copiado y pegado sí corre

--- script.1.sh
+++ script.1.sh
@@ -1,17 +1,21 @@
 # Escriba su código aquí
 ##
+##
 for a in $(seq 1 3)
-do (sed 's/\t/,/g' data${a}.csv | sed '/^[[:space:]]*$/d') > dataN${a}.csv
+do
+    (sed 's/\t/,/g' data${a}.csv | sed '/^[[:space:]]*$/d') > dataN${a}.csv
 done
 ###
 for a in $(seq 1 3) 
-do  cont=0 
+do  
+    cont=0 
     for row in $(cat dataN${a}.csv) 
-    do cont=$((cont+1))
+    do 
+        cont=$((cont+1))
         fila=($( echo ${row[*]} | sed 's/,/\t/g'))
         for x in $( echo ${fila[*]} | sed 's/[A-Z]//' )
-        do  echo data${a}.csv','$cont','${fila[0]}','$x >> out.3 
+        do 
+                echo data${a}.csv','$cont','${fila[0]}','$x >> out.3
         done 
     done
 done
-cat out.3
